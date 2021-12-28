94ff795836060c5f573648b45e07d9b16091f682
Guilherme Pereira de Carvalho <87571252+guipc99@users.noreply.github.com>
2021-12-28 16:01:26 -0300

    feat jasmine e karma completo

--- jasmine-karma-material/src/app/posts/components/list-posts/list-posts.component.spec.ts
+++ jasmine-karma-material/src/app/posts/components/list-posts/list-posts.component.spec.ts
@@ -1,38 +1,40 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatTableModule } from '@angular/material/table';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PostModel } from '../../models/post.model';
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
 import { FormPostsComponent } from '../form-posts/form-posts.component';
 
 import { ListPostsComponent } from './list-posts.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostModel } from '../../models/post.model';
+import { PostService } from '../../services/post.service';
+import { of } from 'rxjs';
+
 describe('ListPostsComponent', () => {
   let component: ListPostsComponent;
   let fixture: ComponentFixture<ListPostsComponent>;
+  let postService: PostService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ListPostsComponent, FormPostsComponent],
       imports: [
         HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+
         MatCardModule,
         MatButtonModule,
         MatSidenavModule,
-        MatFormFieldModule,
+
         MatInputModule,
         MatTableModule,
-        MatSliderModule,
-        FormsModule,
-        ReactiveFormsModule,
-        BrowserAnimationsModule
       ],
     }).compileComponents();
   });
@@ -46,26 +48,137 @@ describe('ListPostsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  //TESTE NO DELETE 
-  it('delete test', ()=>{
-      //criando cenário
-      let post = new PostModel();
-      post.body = 'nome';
-      post.title = 'title';
-      post.userId = 1;
-      post.id = 1;
-      //adiciona o fim do obj no fim do array 
-      component.dataSource.push(post);
-      //tamanho do array antes de ser deletado 
-      let lengthOld = component.dataSource.length;
 
-      //execução
-      component.onDelete(post)
+  it('delete test when found element', () => {
+    // cenário
 
-      //validação
-      expect(component.dataSource.length).toBe(lengthOld - 1)
+    //criando obj post
+    let post = new PostModel();
+    post.body = 'body';
+    post.title = 'title';
+    post.userId = 1;
+    post.id = 1;
 
-  })
+    // adicionei o obj Post no final do array
+    component.dataSource.push(post);
 
+    //tamanho do array antes de deletar
+    let lengthOld = component.dataSource.length;
 
+    // execução
+    component.onDelete(post);
+
+    // validação
+    expect(component.dataSource.length).toBe(lengthOld - 1);
+  });
+
+  it('delete test when not found element', () => {
+    // cenário
+
+    //criando obj post
+    let post = new PostModel();
+    post.body = 'body';
+    post.title = 'title';
+    post.userId = 1;
+    post.id = 1;
+
+    // adicionei o obj Post no final do array
+    component.dataSource.push(post);
+
+    //tamanho do array antes de deletar
+    let lengthOld = component.dataSource.length;
+
+    // clonar variável post para a variável postDelete
+    let postDelete = Object.assign({}, post);
+    postDelete.id = 2;
+
+    // execução
+    component.onDelete(postDelete);
+
+    // validação
+    expect(component.dataSource.length).toBe(lengthOld);
+  });
+  //QUANDO NAO É RECEBIDO UM ID
+  it('when saveOrUpdatePost receive a new post model and dataSource is Empty', () => {
+    // cenário
+    let post = {
+      title: 'title',
+      body: 'body',
+      userId: 1,
+    };
+
+    //tamanho do array antes de inserir
+    let lengthOld = component.dataSource.length;
+
+    // execução
+    component.saveOrUpdatePost(post);
+
+    // validação
+    expect(component.dataSource.length).toBe(lengthOld + 1);
+  });
+  //QUANDO É RECEBIDO UM ID
+  it('when saveOrUpdatePost receive a non empty dataSource', () => {
+    let post = new PostModel();
+    post.body = 'body';
+    post.title = 'title';
+    post.userId = 1;
+    post.id = 1;
+
+    component.dataSource.push(post);
+
+    let postNew = {
+      title: 'title',
+      body: 'body',
+      userId: 1,
+    };
+
+    let lengthOld = component.dataSource.length;
+
+    component.saveOrUpdatePost(postNew);
+
+    expect(component.dataSource.length).toBe(lengthOld + 1);
+  });
+  it('when saveOrUpdatePost receives an existing data', () => {
+    let post = new PostModel();
+    post.body = 'body';
+    post.title = 'title';
+    post.userId = 1;
+    post.id = 1;
+
+    component.dataSource.push(post);
+
+    let postEdit = {
+      title: 'title 2',
+      body: 'body 2',
+      userId: 1,
+      id: 1,
+    };
+
+    let lengthOld = component.dataSource.length;
+
+    component.saveOrUpdatePost(postEdit);
+
+    expect(component.dataSource.length).toBe(lengthOld);
+  });
+  it('getAll', () => {
+    //cenário
+    postService = TestBed.inject(PostService);
+
+    let postResponse : PostModel[] = [
+      {
+        title: 'title',
+        body: 'body',
+        id: 1,
+        userId: 1
+      }
+    ]
+
+    spyOn(postService, 'getAll').and.returnValue( of(postResponse) );
+    //execução
+    component.ngOnInit();
+    //validação
+    //se o dataSource for igual a postResponse, então o getAll do componente foi
+    //executado com sucesso 
+    expect(component.dataSource).toBe(postResponse)
+  });
 });
